6494f7484ee2f5774610d7ec8a385ac5e3db01df
Andreas Klapperich <Andreas@Klapperich.de>
2017-12-11 15:30:54 +0100

    Test value from args, fixes

--- CheckFirewallIsRunning/Program.cs
+++ CheckFirewallIsRunning/Program.cs
@@ -10,7 +10,7 @@ namespace CheckFirewallIsRunning
 {
     class Program
     {
-        static bool IsTest = false;
+        static bool IsTest = Argument<bool>("Test");
         static TimeSpan StatusTimeout = IsTest ? TimeSpan.FromSeconds(10) : TimeSpan.FromMinutes(1);
         static TimeSpan StartTimeout = TimeSpan.FromMinutes(2);
         static ServiceController FirewallService = new ServiceController("MpsSvc");
@@ -44,17 +44,19 @@ namespace CheckFirewallIsRunning
         {
             while (!Running.IsCancellationRequested)
             {
-                await Task.Delay(statusTimeout ?? StatusTimeout, Running.Token).ConfigureAwait(false);
+                await Task.Delay(statusTimeout ?? StatusTimeout, Running.Token);
                 if (FirewallService.Status == status)
                     break;
             }
         }
 
-        static async Task WaitForLeaveFirewallStatus(ServiceControllerStatus status, TimeSpan? statusTimeout = default)
+        static async Task WaitForLeaveFirewallStatus(ServiceControllerStatus status, TimeSpan? statusTimeout = default, int maxRuns = 5)
         {
+            int run = 0;
             while (!Running.IsCancellationRequested)
             {
-                await Task.Delay(statusTimeout ?? StatusTimeout, Running.Token).ConfigureAwait(false);
+                if (++run > maxRuns) return;
+                await Task.Delay(statusTimeout ?? StatusTimeout, Running.Token);
                 if (FirewallService.Status != status)
                     break;
             }
@@ -69,6 +71,30 @@ namespace CheckFirewallIsRunning
                 FirewallService.Start();
                 await WaitForFirewallServiceStatus(ServiceControllerStatus.Running, StartTimeout);
             }
+        }
+
+        /// <summary>
+        /// Commandline Key=Value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static T Argument<T>(string key)
+        {
+            try
+            {
+                foreach (var a in Environment.GetCommandLineArgs())
+                {
+                    var kv = a.Split('=');
+                    if (kv.Length == 2 && kv[0] == key)
+                        return (T)Convert.ChangeType(kv[1], typeof(T));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return default;
         }
     }
 }
